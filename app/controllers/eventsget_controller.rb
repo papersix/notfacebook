c973ee5b536fce115e78cf75d79e2a105c8b4f26
Rahul Alexander <alexander@chroma.org>
2016-08-11 14:31:43 -0400

    returning API data

--- app/controllers/eventsget_controller.rb
+++ app/controllers/eventsget_controller.rb
@@ -1,12 +1,12 @@
 class EventsgetController < ApplicationController
 
   def index
-  #some way to access facebook & get the json back - http party? koala?
-    # access_token = request.env['omniauth.auth']
-    # facebook = Koala::Facebook::API.new(access_token)
-    # response = facebook.get_object("me?fields=events")
-    # parsed_body = JSON.parse(response.body)
-    # render json: parsed_body
+  # some way to access facebook & get the json back - http party? koala?
+    access_token = request.env['omniauth.auth']
+    facebook = Koala::Facebook::API.new(access_token)
+    response = facebook.get_object("me?fields=events")
+    parsed_body = JSON.parse(response.body)
+    render json: parsed_body
 
   end
 
