b3039f625a183baa17c7903db85b7b188197a307
Rahul Alexander <alexander@chroma.org>
2016-08-15 00:53:59 -0400

    another commit

--- app/controllers/eventsget_controller.rb
+++ app/controllers/eventsget_controller.rb
@@ -2,7 +2,7 @@ class EventsgetController < ApplicationController
 
   def index
   # some way to access facebook & get the json back - http party? koala?
-    access_token = request.env['omniauth.auth']
+    access_token = request.env['<omniauth class="auth"></omniauth>']
     facebook = Koala::Facebook::API.new(access_token)
     response = facebook.get_object("me?fields=events")
     parsed_body = JSON.parse(response.body)
